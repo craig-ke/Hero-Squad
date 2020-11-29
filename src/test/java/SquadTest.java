4b36818112fc31ace4016b71e4a989ea8eb5ba19
craig-ke <lawremce.olonyi.67@gmail.com>
2020-11-29 15:41:57 +0300

    second Quad test PAss

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -1,11 +1,23 @@
 import org.junit.After;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class SquadTest {
     @After
+    public void tearDown() {
+        Squad.clear();
+    }
+
+    @Test
     public void squad_instantiatesCorrectly_true() {
         Squad testSquad = new Squad("Avengers",12,"Protect Earth");
         assertEquals(true, testSquad instanceof Squad);
     }
+    @Test
+    public void getName_returnsSquadName_String() {
+        Squad testSquad = new Squad("Avengers",12,"Protect Earth");
+        assertEquals("Avengers", testSquad.getName());
+    }
+
 }
